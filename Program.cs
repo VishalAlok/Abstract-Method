9473fbc3a5d03f4863c32e51954e6a672507af14
vishal.Alok <you@example.com>
2021-10-17 23:06:20 +0530

    Updated Code

--- Program.cs
+++ Program.cs
@@ -2,15 +2,15 @@
 
 namespace Abstract_Method
 {
-	Abstract class Class1
-	{
-		public abstract void Show() //abstract forces child to override
-}
-	class Claass2 : Class 1
-{
-		public override void Show()
-		{
-			//Implementing Show() Method
-		}
-	}
+    abstract class Class1
+    {
+        public abstract void Show(); //abstract forces child to override
+    }
+    class Claass2 : Class1
+    {
+        public override void Show()
+        {
+            //Implementing Show() Method
+        }
+    }
 }
