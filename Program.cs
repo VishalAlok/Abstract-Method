952a6dc7b6494e1e46f4a26b727d48057224a2bd
vishal.Alok <you@example.com>
2021-10-17 23:05:09 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Abstract_Method
+{
+	Abstract class Class1
+	{
+		public abstract void Show() //abstract forces child to override
+}
+	class Claass2 : Class 1
+{
+		public override void Show()
+		{
+			//Implementing Show() Method
+		}
+	}
+}
